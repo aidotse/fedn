c817f9768b87865924b40cffd5d87c377f53c1f7
Sheetalr24 <sheetal.reddy@ai.se>
2020-10-26 16:16:13 +0100

    remove yolov3 from import

--- fedn/fedn/utils/yolo3/model.py
+++ fedn/fedn/utils/yolo3/model.py
@@ -18,7 +18,7 @@ from keras.layers import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
 
-from yolo3.utils import compose
+from utils import compose
 
 
 @wraps(Conv2D)
