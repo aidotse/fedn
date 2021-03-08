eaf20839a90fb6892232999c8c2c739357f48391
Sheetalr24 <sheetal.reddy@ai.se>
2021-03-08 12:50:05 +0100

    Adding get_data for retrieving mAP

--- test/fedbird/client/yolo3/utils.py
+++ test/fedbird/client/yolo3/utils.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : utils.py
+# Author            : Sheetal Reddy <sheetal.reddy@ai.se>
+# Date              : 08.03.2021
+# Last Modified Date: 08.03.2021
+# Last Modified By  : Sheetal Reddy <sheetal.reddy@ai.se>
 """Miscellaneous utility functions."""
 
 from functools import reduce
@@ -33,6 +40,40 @@ def letterbox_image(image, size):
 
 def rand(a=0, b=1):
     return np.random.rand()*(b-a) + a
+
+
+def get_data(annotation_line, input_shape, random=True, max_boxes=20, jitter=.3, hue=.1, sat=1.5, val=1.5, proc_img=True):
+    annotation_line = annotation_line
+    line = annotation_line.split()
+    image = Image.open(line[0])
+    iw, ih = image.size
+    h, w = input_shape
+    box = np.array([np.array(list(map(int,box.split(',')))) for box in line[1:]])
+
+    if not random:
+        # resize image
+        scale = min(w/iw, h/ih)
+        nw = int(iw*scale)
+        nh = int(ih*scale)
+        dx = (w-nw)//2
+        dy = (h-nh)//2
+        image_data=0
+        if proc_img:
+            image = image.resize((nw,nh), Image.BICUBIC)
+            new_image = Image.new('RGB', (w,h), (128,128,128))
+            new_image.paste(image, (dx, dy))
+            image_data = np.array(new_image)/255.
+
+        # correct boxes
+        box_data = np.zeros((max_boxes,7))
+        if len(box)>0:
+            np.random.shuffle(box)
+            if len(box)>max_boxes: box = box[:max_boxes]
+            box[:, [0,2]] = box[:, [0,2]]*scale + dx
+            box[:, [1,3]] = box[:, [1,3]]*scale + dy
+            box_data[:len(box), 0:5 ] = box
+        return image_data, box_data[~np.all(box_data == 0, axis=1)]
+
 
 def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jitter=.3, hue=.1, sat=1.5, val=1.5, proc_img=True):
     '''random preprocessing for real-time data augmentation'''
